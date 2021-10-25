686f06309ecfc1706278348903ddbe759398b1e1
mattmerovitz <matthew.merovitz1@gmail.com>
2021-10-25 12:30:44 -0400

    Changes

--- Pupillometry_Data_Analysis_StarterCode/2ProcessingCodes/PreProcessing/extracttrials.m
+++ Pupillometry_Data_Analysis_StarterCode/2ProcessingCodes/PreProcessing/extracttrials.m
@@ -3,25 +3,26 @@ function [screens1,screens2,trials] = extracttrials(dataTable)
     % Variables to output
     screens1 = table;
     screens2 = table;
-    trials{96} = [];
+    trials{8} = [];
 
     % Variables
-    numSections = 98;
+    numSections = 85;
     trialIndexCol = 3;
+    trialnoCol = 2;
     % Col 6 = LeftPupil; Col 7 = RightPupil; Col 8 = SampleMessages
-    colsToInclude = [6 7 8];
-    screensColsToInclude = [6 7 8];
+    colsToInclude = [5 6 7 9 10 11 12];
+    screensColsToInclude = [9 10 11 12];
     
     % Separate screen data
     screens1Rows = (dataTable{:,trialIndexCol} == 1);
     screens1 = dataTable(screens1Rows, screensColsToInclude);
     
-    screens2Rows = (dataTable{:,trialIndexCol} == 98);
+    screens2Rows = (dataTable{:,trialIndexCol} == numSections);
     screens2 = dataTable(screens2Rows, screensColsToInclude);
     
     % Add each trial section to corresponding cell in trials cell array
-    for i = 2:numSections-1
+    for i = 1:size(trials,1)
         disp(i)
-        rowsInSection = (dataTable{:,trialIndexCol} == i);
-        trials{i-1} = dataTable(rowsInSection, colsToInclude);
+        rowsInSection = ((dataTable{:,trialnoCol} == i) && ~(dataTable{:,trialIndexCol} == 1) && ~(dataTable{:,trialIndexCol} == numSections));
+        trials{i} = dataTable(rowsInSection, colsToInclude);
     end
